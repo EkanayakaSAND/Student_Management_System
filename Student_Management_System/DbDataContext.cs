b2b4ce9072436598f059967195f17c32b670d8a6
Nirmana Dasunpriya Ekanayka <ndasunpriya.nd@gmail.com>
2023-10-28 10:40:59 +0530

    Added screenshots

--- Student_Management_System/DbDataContext.cs
+++ Student_Management_System/DbDataContext.cs
@@ -1,18 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Student_Management_System.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Student_Management_System
 {
     public class DbDataContext : DbContext
     {
         //Change this path to your database full path
-        private readonly string _path = @"F:\UOR\FacultyOfEngineering\Semester_03\Projects\Student_Management_System\Student_Management_System\Database.db";
+        private readonly string _path = @"F:\UOR\FacultyOfEngineering\Projects\Student_Management_System\Student_Management_System\Database.db";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
